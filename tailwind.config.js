8e37e79dfb2cff1aba84abfae1372e6d345c4e30
Azy <56982188+azycat@users.noreply.github.com>
2023-11-03 00:10:15 -0700

    fix instantclick markdown rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
       fontFamily: {
         sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
-        mono: ["Fira\\ Code", ...defaultTheme.fontFamily.mono],
+        // mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono],
       },
     },
   },
